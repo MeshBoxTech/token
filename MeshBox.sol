3c3f554751218046292782b5ca545f4c6611e406
MeshBox <45959616+MeshBoxFoundation@users.noreply.github.com>
2020-08-14 15:05:11 +0800

    Update MeshBox.sol

--- MeshBox.sol
+++ MeshBox.sol
@@ -276,7 +276,7 @@ contract MeshBox is IERC20,Controlled {
   */
   function transfer(address to, uint256 value) transferAllowed(msg.sender) public returns (bool) {
     require(value <= _balances[msg.sender]);
-
+    //require(to != address(0)); allow to destroy mesh token to 0x0000000000000000000000000000000000000000
     _balances[msg.sender] = _balances[msg.sender].sub(value);
     _balances[to] = _balances[to].add(value);
     emit Transfer(msg.sender, to, value);
